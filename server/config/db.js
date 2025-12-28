ef20ac8a7803807888dd4072b3f7e82362a2ced4
Adarsha kumar Maharana <eadarsha2003@gmail.com>
2025-12-28 23:32:55 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,16 @@
 import mysql from "mysql2/promise";
-import dotenv from "dotenv";
-dotenv.config({ path: "./server/.env" });
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false
+  },
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
